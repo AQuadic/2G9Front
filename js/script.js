3b5da9055d8fbb77e814e7ab5c6240e50411ba99
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2024-09-02 15:24:20 +0300

    edits

--- js/script.js
+++ js/script.js
@@ -8,3 +8,24 @@ document.addEventListener('DOMContentLoaded', () => {
         toggleButton.setAttribute('aria-expanded', !isExpanded);
     });
 });
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    loop:true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 150,
+      modifier: 2.5,
+      slideShadows: true,
+    },
+    autoplay:{
+  
+      delay:3000,
+      disableOnInteraction:false,
+    }
+  
+  });
