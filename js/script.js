75f03cb52e6fae4ad952ecc57dcb7cc7b23ccb34
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2024-09-08 16:41:32 +0300

    add pop up

--- js/script.js
+++ js/script.js
@@ -9,6 +9,41 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+
+// Swap Buttons in Index
+function filterCards(category) {
+const cards = document.querySelectorAll('.card');
+const buttons = document.querySelectorAll('.filter-btn');
+
+buttons.forEach(btn => {
+if (btn.textContent.trim() === getButtonText(category)) {
+    btn.classList.remove('bg-transparent', 'text-black', 'border-[#C10D10]');
+    btn.classList.add('bg-[#C10D10]', 'text-white');
+} else {
+    btn.classList.remove('bg-[#C10D10]', 'text-white');
+    btn.classList.add('bg-transparent', 'text-black', 'border-[#C10D10]');
+}
+});
+
+cards.forEach(card => {
+    if (category === 'all' || card.dataset.category === category) {
+        card.classList.remove('hidden');
+    } else {
+        card.classList.add('hidden');
+    }
+});
+}
+
+function getButtonText(category) {
+switch(category) {
+    case 'all': return 'الكل';
+    case 'ads': return 'أبرز الإعلانات التجارية';
+    case 'personal': return 'أبرز الإعلانات الشخصية';
+    default: return '';
+}
+}
+
+// Swiper
 var swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
@@ -16,16 +51,16 @@ var swiper = new Swiper(".mySwiper", {
     loop:true,
     slidesPerView: "auto",
     coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 150,
-      modifier: 2.5,
-      slideShadows: true,
-    },
-    autoplay:{
-  
-      delay:3000,
-      disableOnInteraction:false,
-    }
-  
-  });
+    rotate: 0,
+    stretch: 0,
+    depth: 150,
+    modifier: 2.5,
+    slideShadows: true,
+},
+autoplay:{
+
+    delay:3000,
+    disableOnInteraction:false,
+}
+
+});
